32ef5a58c2585e8b704f548f833d4170acd380a0
Napat Srichan <mail@napatsc.com>
2025-05-20 00:13:55 -0700

    add leaflet maps

--- Website/src/lib/nocodb.ts
+++ Website/src/lib/nocodb.ts
@@ -1,6 +1,6 @@
 export const nocodbHost = import.meta.env.NOCODB_URL;
-const token = import.meta.env.NOCODB_TOKEN;
-const tableId = import.meta.env.NOCODB_TABLEID;
+export const token = import.meta.env.NOCODB_TOKEN;
+export const tableId = import.meta.env.NOCODB_TABLEID;
 
 const arrayToQuery = (key: string, vals?: string[]) =>
   vals !== undefined && vals.length > 0 ? `${key}=${vals.join(",")}` : "";
@@ -9,13 +9,15 @@ export const read = async ({
   id,
   fields,
   view,
+  table
 }: {
   id?: string;
   fields?: string[];
   view?: string;
+  table?: string;
 }): Promise<any[]> => {
   const res = await fetch(
-    `${nocodbHost}/api/v2/tables/${tableId}/records?${id !== undefined ? `where=%28participant_code%2Ceq%2C${id}%29&` : ""}limit=999&${arrayToQuery("fields", fields)}${view ? `&viewId=${view}` : ""}`,
+    `${nocodbHost}/api/v2/tables/${table||tableId}/records?${id !== undefined ? `where=%28participant_code%2Ceq%2C${id}%29&` : ""}limit=999&${arrayToQuery("fields", fields)}${view ? `&viewId=${view}` : ""}`,
     {
       headers: {
         accept: "application/json",
@@ -24,13 +26,5 @@ export const read = async ({
     },
   );
   if (!res.ok) return Promise.reject(res.statusText);
-  const list = (await res.json()).list.map((i) => {
-    delete i.Id;
-    return i;
-  });
-  return list;
+  return (await res.json()).list;
 };
-
-// const res = await read({ id: "1" })
-// const res = await read({ fields: ["participant_code", "sex", "speaker_group", "language_dominance"] })
-// console.log(res)
